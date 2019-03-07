23ad020e53418c0081043f0865772c4d696e5dc5
ramirogavagnin <ramirogavagnin@gmail.com>
2019-03-07 10:09:47 -0300

    Landinggg ends :)

--- js/index.js
+++ js/index.js
@@ -1,32 +1,5 @@
-console.log('Landing')
-// Validar el email ;D
 
-var emailInputNode = document.getElementById('email')
-
-emailInputNode.onblur = validateInputEmail
-
-
-function validateInputEmail(event){
-
-var inputNode = event.target
-
-
-if (!inputNode.value || 
-  inputNode.value.indexOf('@') === -1 || 
-  inputNode.value.indexOf('.') === -1) 
-  {
-    inputNode.classList.remove('is-valid')
-    inputNode.classList.add('is-invalid')
-
-  } else{  
-    inputNode.classList.remove('is-invalid')
-    inputNode.classList.add('is-valid')
-      }  
-
-      validateButton()
-
-    }
-
+console.log('landing')
 
 
 
